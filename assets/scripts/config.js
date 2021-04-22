bf2952cf9054a0d2fd2ccf83a7c398613fb4beb6
Angel-Journey <angel.journey7@gmail.com>
2021-04-22 14:46:41 -0500

    Submit design progress
    Header, buttons bar, and background colors

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://whim-vim-api.herokuapp.com',
-  development: 'https://whim-vim-api.herokuapp.com'
+  development: 'http://localhost:4741'
 }
 
 // development: 'http://localhost:4741'
